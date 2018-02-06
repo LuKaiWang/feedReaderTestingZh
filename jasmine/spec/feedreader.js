b5895eaecd513a2d5e94195bb4b3a26bb04151ae
546417331@qq.com <546417331@qq.com>
2018-02-06 16:36:05 +0800

    修改异步函数测试部分bug

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,14 +4,13 @@
                 我们得保证在 DOM 准备好之前他们不会被运行。
  */
 $(function() {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000; //网络延迟严重，更改默认间隔时间为10秒
-  const deInterval = 8000; //网络延迟，设置8秒，保证函数执行完成
   /**
    * @description 这是一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
                   都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
    */
   //let oldContent;
   describe('RSS Feeds', function() {
+    const regularExpressionUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/; // 检查 URL 格式是否正确的正规表达式
     /**
      *@description 这是一个测试 - 它用来保证 allFeeds 变量被定义了而且不是空的。
      */
@@ -19,13 +18,13 @@ $(function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
-
     /**
      * @description 这是一个测试，遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
      */
     it('every object of allFeeds has url-field and it is not null', function() {
       for (let feed of allFeeds) {
         expect(feed.url.length).not.toBe(0);
+        expect(feed.url).toMatch(regularExpressionUrl); //检查url格式
       }
     });
 
@@ -47,7 +46,7 @@ $(function() {
      * @description 这是一个测试用例保证菜单元素默认是隐藏的。默认body标签类为'menu-hidden'。
      */
     it('the menu defualt hide', function() {
-      expect(document.querySelector("body").getAttribute('class')).toBe('menu-hidden');
+      expect($("body").hasClass("menu-hidden")).toBe(true);
     })
     /**
      * @description 这是一个测试，保证当菜单图标被点击的时候菜单会切换可见状态。这个
@@ -55,13 +54,11 @@ $(function() {
                     再次点击的时候是否隐藏。
      */
     it('clicking menu-icon can change visible status', function() {
-      let body = document.querySelector("body"); //获取body元素
-      let icon = document.querySelector(".icon-list"); //获取list图标
-      body.setAttribute('class', 'menu-hidden'); //body默认隐藏
-      icon.click(); //点击后显示
-      expect(body.getAttribute('class')).toBe('');
-      icon.click(); //再次点击后隐藏
-      expect(body.getAttribute('class')).toBe('menu-hidden');
+      $("body").addClass('menu-hidden'); //body默认隐藏
+      $(".icon-list").click(); //点击后显示
+      expect($("body").hasClass("menu-hidden")).toBe(false);
+      $(".icon-list").click(); //再次点击后隐藏
+      expect($("body").hasClass("menu-hidden")).toBe(true);
     });
   });
 
@@ -74,13 +71,10 @@ $(function() {
                     里面至少有一个 .entry 的元素。
      */
     beforeEach(function(done) {
-      loadFeed(0);
-      setTimeout(function() {
-        done();
-      }, deInterval);
+      loadFeed(0, done);
     });
     it('loadFeed can work', function(done) {
-      expect(document.querySelector('.feed').children[0].children[0].getAttribute('class')).toBe('entry');
+      expect($('.entry').length).not.toBe(0);
       done();
     });
   });
@@ -93,20 +87,14 @@ $(function() {
     let newContent; //新源第一个标题
     /**
      *@description 这是一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-                   记住，loadFeed() 函数是异步的。
      */
     beforeEach(function(done) {
-      //获取旧源第一个标题
-      oldContent = document.querySelector('.feed').children[0].children[0].children[0].textContent;
+      oldContent = $('.feed .entry:first').text(); //获取旧源第一个标题
       let id = Math.round(Math.random() * 2) + 1; //1--3之间之间随机整数
-      loadFeed(id); //加载新源
-      setTimeout(function() {
-        done();
-      }, deInterval);
+      loadFeed(id, done); //加载新源
     });
     it('when add new source,the content will change', function(done) {
-      //获取新源第一个标题内容
-      newContent = document.querySelector('.feed').children[0].children[0].children[0].textContent;
+      newContent = $('.feed .entry:first').text(); //获取新源第一个标题内容
       expect(oldContent).not.toBe(newContent); //旧源标题与新源标题不一样
       done();
     });
